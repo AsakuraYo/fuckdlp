2315b2c27536c676385a132f3dff8bf8a7911d08
Jianqiang Liang <jianqiang070@foxmail.com>
2013-12-19 01:43:43 +0800

    join two bat files to one

--- bin/fuckdlp.bat
+++ bin/fuckdlp.bat
@@ -1,4 +1,12 @@
 @echo off
 :: echo "%~dp0"
-"%~dp0\javaw.exe" %1
-"%~dp0\renamefile.exe" %1.txt %1
+if exist %1\.\ (
+    for /R %1 %%s in (*) do (
+        :: echo %%s
+        "%~dp0\javaw.exe" "%%s"
+        "%~dp0\renamefile.exe" "%%s.txt" "%%s"
+    )
+) else (
+    "%~dp0\javaw.exe" %1
+    "%~dp0\renamefile.exe" %1.txt %1
+)
--- bin/fuckdlpdir.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-for /R %1 %%s in (*) do (
-    :: echo %%s
-    "%~dp0\javaw.exe" "%%s"
-    "%~dp0\renamefile.exe" "%%s.txt" "%%s"
-)
